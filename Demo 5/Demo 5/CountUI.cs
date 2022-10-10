1c0abd2e2bd22599e5ea9359cfb464489a55129b
Madman283 <93745722+Madman283@users.noreply.github.com>
2022-10-10 14:12:48 -0500

    uh

---
+++ Demo 5/Demo 5/CountUI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Demo_5
+{
+    internal class CountUI
+    {
+        Counter counter;
+        public CountUI() => SetUp();
+
+        private void SetUp()
+        {
+            Clear();
+            Title = "Event example with a counter and threshold";
+            WriteLine("A random number has been chosen as a threshold.\nNumbers will increment automatically until this threshold has been met.");
+            counter = new Counter(GetRandomNumber());
+            Run();
+        }
+        private int GetRandomNumber()
+        {
+            return new Random().Next(10);
+        }
+
+        private void Run()
+        {
+            WriteLine(IncrementCounter());
+            counter.ThresholdReached += c_ThresholdReached;
+            Run();
+        }
+        private int IncrementCounter()
+        {
+            counter.Add(1);
+            return counter.Total;
+        }
+
+        private void c_ThresholdReached(object sender, EventArgs e)
+        {
+            WriteLine($"The threshold {counter.Threshold} was reached. Press any key to run this again, or x to exit.");
+            if (Console.ReadKey().Key == ConsoleKey.X)
+                Environment.Exit(0);
+            //else
+            SetUp();
+        }
+    }
+}
---
+++ Demo 5/Demo 5/Counter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Demo_5
+{
+    internal class Counter
+    {
+        private int threshold; private int total;
+        public event EventHandler ThresholdReached;
+        public int Total { get => total; set => total = value; }
+        public int Threshold { get => threshold; set => threshold = value; }
+
+        public Counter(int passedThreshold) => threshold = passedThreshold;
+
+        public void Add(int x)
+        {
+            total += x;
+            if (total >= threshold)
+                OnThresholdReached(EventArgs.Empty);
+        }
+
+        protected virtual void OnThresholdReached(EventArgs e)
+        {
+            EventHandler handler = ThresholdReached;
+            if (handler != null)
+                handler(this, e);
+        }
+    }
+}
